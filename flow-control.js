b754d6c11750b8b80f494cec9c57e3ad4f80d0a6
Joshua Choi <jsc.r839@gmail.com>
2019-06-06 21:22:15 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -6,14 +6,22 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  if ( age > 13 && age < 19)
+  if ( age > 13 && age < 19) {
     return console.log("You are a teenager!")
-
+  } else{
+    return console.log("You are not a teenager")
+  }
 
 }
 
 function ageChecker(age) {
-
+  if ( age > 13 && age < 19) {
+    return console.log("You are a teenager!")
+  } elseif ( age <= 12){
+    return console.log("You are a kid")
+  } elseif ( age >= 20){
+    return console.log("You are a grownup")
+  }
 }
 
 function ternaryTeenager(age) {
